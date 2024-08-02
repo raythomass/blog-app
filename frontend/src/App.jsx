d805424d14a4f720581c49543d41fbfcc8439161
Ray Thomas <rthomas5678@gmail.com>
2024-08-01 20:11:37 -0700

    added nav and routes from nav items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer"
 import Nav from "./components/Nav"
+import './App.css'
 
 const App = () => {
   return (
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Nav  = () => {
+    const user = true;
     return(
-        <div>
-            Nav
+        <div className="nav">
+            <div className="nav-items flex justify-between">
+                <Link className="logo" to='/'>Blog</Link>
+                <div className="nav-btns flex gap-5">
+                    <input placeholder="Search a post" type="text"></input>
+                    {user? <Link to='/write'>Write</Link> :<Link to='/login'>Login</Link>}
+                    {user? <Link to='/profile'>Profile</Link> :<Link to='/registar'>Registar</Link>}
+                </div>
+            </div>
         </div>
     )
 }
