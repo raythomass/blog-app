e9e6a567e7c693454441c7e5b0c88c02065237c7
Ray Thomas <rthomas5678@gmail.com>
2024-08-01 21:15:36 -0700

    layout for homepage posts

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,6 @@
 const Footer  = () => {
     return(
-        <div>
-            Footer
-        </div>
+        <div className="footer"></div>
     )
 }
 
--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,9 +1,25 @@
 const HomePosts  = () => {
     return(
         <div className="homeposts">
-            <img className="homeposts-img"></img>
-            <div className="homeposts-details"></div>
-            
+            <div className="homeposts-cont">
+                <div className="homeposts-img-cont">
+                    <img className="homeposts-img" src="https://picsum.photos/seed/picsum/400/200"></img>
+                </div>
+                <div className="homeposts-details">
+                    <h2>Blog Post Title Example</h2>
+                    <div className="homeposts-info">
+                        <p>11:30PM</p>
+                        <p>08/01/2024</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                           ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                           voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                           officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
         </div>
 
     )
